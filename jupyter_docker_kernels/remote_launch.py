73492ec98b093745830ec173e3a02aa09d418712
Thomas Kluyver <thomas@kluyver.me.uk>
2018-04-14 12:00:43 +0200

    Remove unused file from ssh implementation

--- jupyter_docker_kernels/remote_launch.py
+++
@@ -1,6 +0,0 @@
-"""Sent to Python on the remote system to launch a kernel."""
-
-from jupyter_client.manager2 import KernelManager2
-km = KernelManager2(kernel_cmd={argv!r}, cwd={cwd!r}, ip={address!r})
-print("!!!Started, connection_file:", km.connection_file, '!!!', flush=True)
-km.wait(None)
